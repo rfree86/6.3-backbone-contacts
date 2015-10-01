d330c9bf1b6c442210250ac7adfa36bf0777ce52
Richard <rfree86@gmail.com>
2015-10-01 13:32:32 -0400

    ending project with validation error

--- app/scripts/models/contact.js
+++ app/scripts/models/contact.js
@@ -10,16 +10,23 @@ var Contact = Backbone.Model.extend({
     };
   },
 
-  // validate: function(attributes) {
-  //   var errors = {};
-  //   if(_.isEmpty(attributes.title)){
-  //     errors.title = "Must have a valid title";
-  //   }
-  //   if(_.isEmpty(attributes.author)){
-  //     errors.author = "Must have a valid author";
-  //   }
-  //   return _.keys(errors).length ? errors : undefined;
-  // }
-})
+  validate: function(attributes) {
+    var errors = {};
+    if(_.isEmpty(attributes.first_name)){
+      return "Must have a first name";
+    }
+    if(_.isEmpty(attributes.last_name)){
+      return "Must have a valid last name";
+    }
+    if(_.isEmpty(attributes.email)){
+      return "Must have a valid email";
+    }
+    if(_.isEmpty(attributes.phone_number)){
+      return "Must have a valid phone number";
+    }
+
+
+  }
+});
 
 export default Contact;
